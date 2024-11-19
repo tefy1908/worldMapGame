bdb1899d53452a67ca69b65685432b071fd75a34
tefy1908 <tefymaherison19@gmail.com>
2024-11-19 17:58:48 +0100

    correction musique

--- sketch.js
+++ sketch.js
@@ -32,12 +32,10 @@ function preload() {
 }
 function setup() {
   createCanvas(windowWidth, windowHeight);
-    // Chargement de la musique 
-    if (music.isLoaded()) {
-        music.setVolume(0.5); // Définir le volume (valeur entre 0 et 1)
-        music.loop(); // Faire en sorte que la musique joue en boucle
-      }
-    }
+  if (music.isLoaded()) {
+    music.setVolume(0.5); // Définir le volume (valeur entre 0 et 1)
+    music.loop(); // Faire en sorte que la musique joue en boucle
+  }
   popupMessage = createDiv().style('background-color', '#fff')
     .style('padding', '10px')
     .style('border-radius', '10px')
